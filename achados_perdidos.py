b7a3e96be7c7db99fb212389888b1c5863c28dee
davihribeiro7-coder <davi.h.ribeiro7@aluno.senai.br>
2026-07-30 16:37:24 -0300

    Update achados_perdidos.py

--- achados_perdidos.py
+++ achados_perdidos.py
@@ -7,3 +7,13 @@ print("2 - Listar objetos")
 print("3 - Excluir objeto")
 print("4 - Sair")
 mostrador_menu()
+
+def mostrar_menu():
+print("\n================================")
+print(" ACHADOS E PERDIDOS DA ESCOLA")
+print("================================")
+print("1 - Cadastrar objeto")
+print("2 - Listar objetos")
+print("3 - Excluir objeto")
+print("4 - Sair")
+main()
